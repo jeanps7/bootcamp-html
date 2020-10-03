cdf53fe107aecf30309e685a4abf3e9ea57e394d
jeanps7 <philip-j1@hotmail.com>
2020-10-03 17:50:20 -0300

    Introdução a Javascript

---
+++ Introdução a Javascript/js/main.js
@@ -0,0 +1,66 @@
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    // alert("Obrigado por clicar")
+}
+
+function Redirecionar(){
+window.open("https://www.google.com")
+window.location.href ="https://www.google.com";
+}
+
+function Trocar(){
+    document.getElementById("mouse").innerHTML = "Obrigado por passar o mouse";
+}
+
+// function soma(n1,n2){
+//     return n1 + n2;
+// }
+// alert(soma(5,10));
+
+// var d = new Date();
+// alert(d.getMonth()+1);
+
+/*var count;
+
+for(count=0;count<=5;count++){
+    alert(count);
+}*/
+
+/*var count = 0;
+while (count <=5){
+    console.log(count);
+    count++;
+};*/
+
+/*var idade=prompt("Qual sua idade");
+
+if (idade >= 18){
+    alert("Maior de idade")
+}else{
+    alert("menor de idade")
+}*/
+
+
+//var frutas= [{nome: "maça", cor:"vermelha"},{nome: "uva", cor:"roxa"}];
+//console.log(frutas[0].nome);
+
+//var fruta= {nome: "maça", cor:"vermelha"};
+//console.log(fruta.nome);
+
+//var lista = ["maça","pera","laranja"];
+//lista.push("uva");
+//lista.push("uva");
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+//var nome = "Jean Philip";
+//var idade = "20";
+//var idade2 = "10";
+//var frase = "Japão é o melhor time do mundo"; 
+//alert(nome);
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(frase.replace("Japão","Brasil"));
+//console.log(frase.toUpperCase());
